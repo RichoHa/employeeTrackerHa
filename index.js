61eedf1ba07a439b52ca76eb96caaf3dc5517b29
Richo Ha <richard.dqh@gmail.com>
2021-05-25 17:16:11 +0800

    Updated work with correct json files

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 //insert required libraries
 const inquirer = require("inquirer");
 const cTable = require("console.table");
-const mysql = require('mysql2');
 
+
+async function connecttoDB() {
+    const mysql = require('mysql2/promise');
+
+    const connection = await mysql.createConnection({
+        host: "127.0.0.1",
+        port: 3306,
+        user: "root",
+        password: "Duc.Ha.93",
+        database: "employeeDB",
+    })
+    const [first_name, last_name, title, department, salary, manager] = await connection.execute('SELECT * FROM `employee`');
+}
+
+connecttoDB();
