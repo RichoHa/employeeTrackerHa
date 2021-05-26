dafdcbed6176dc1583426e8d963e73b25d58dfde
Richo Ha <richard.dqh@gmail.com>
2021-05-26 22:31:33 +0800

    Added department question

--- add.js
+++ add.js
@@ -10,6 +10,11 @@ let employeeArray = [
     { "id": "3", "first_name": "Daniel", "last_name": "Ha", "role_id": "Engineer", "manager_id": "Richo Ha" }
 ]
 
+let departmentArray = [
+    { "id": "1", "name": "Engineering" },
+    { "id": "2", "name": "Pharmacy" }
+]
+
 //Questions Regarding adding departments, roles and employees.
 async function add() {
     const response = await inquirer.prompt([
@@ -60,7 +65,31 @@ async function add() {
             }
         ]);
 
+        //get details and uploaded into database
+        console.log(`added ${response.employeeFirstName} ${response.employeeLastName} with role ${response.employeeRole} and manager ${response.employeeManger}`)
+
+
     } else if (response.addItem === "Add a department") {
+        //Map all name of deparmtments
+        let departmentArrayMapped = departmentArray.map(a => a.name);
+
+        const response = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'addedDepartment',
+                message: 'What is the department you want to add?',
+                //Validate if alread exists.
+                validate: (addedDepartment) => {
+                    if (!departmentArrayMapped.includes(addedDepartment)) {
+                        return true;
+                    } else {
+                        console.log(`(Error: This department already exists!)`);
+                    }
+                }
+            }
+        ])
+
+        console.log(`Department ${response.addedDepartment} was added`);
 
     } else {
 
