0c55c7e185040cbb48353eb52ac217c0c8c9ce12
Richo Ha <richard.dqh@gmail.com>
2021-05-30 15:15:14 +0800

    Add functionality all completed

--- assets/js/add.js
+++ assets/js/add.js
@@ -14,17 +14,23 @@ async function add(roleArray, employeeArray, departmentArray) {
     ]);
 
     if (response.addItem === "Add an Employee") {
-        //get all the roles within the role table/object
-        let getAllRoles = roleArray.map(a => a.title);
 
-        //gets the single instance of manager within the Array/object 
-        let getAllManagers = employeeArray.map(a => a.manager_id);
-        let allManagers = [];
-        getAllManagers.forEach(Manager => {
-            if (!allManagers.includes(Manager)) {
-                allManagers.push(Manager)
-            }
-            return allManagers;
+        const connection = await mysql.createConnection(connectionFile.connection);
+
+        //Get Roles into an array
+        const roleTable = await connection.query(
+            `SELECT id, title FROM role`);
+        let listOfRoleTitles = [];
+        roleTable[0].forEach(element => {
+            listOfRoleTitles.push(element.title)
+        })
+
+        //Get Manager's Names into an array
+        const managerTable = await connection.query(
+            `SELECT id, manager_name FROM manager`);
+        let listOfmanagerNames = [];
+        managerTable[0].forEach(element => {
+            listOfmanagerNames.push(element.manager_name)
         })
 
         const response = await inquirer.prompt([
@@ -42,23 +48,53 @@ async function add(roleArray, employeeArray, departmentArray) {
                 type: 'list',
                 name: 'employeeRole',
                 message: 'What is the employee’s role? ',
-                choices: getAllRoles
+                choices: listOfRoleTitles
             },
             {
                 type: 'list',
                 name: 'employeeManger',
                 message: 'What is the employee’s manager? ',
-                choices: allManagers
+                choices: listOfmanagerNames
             }
         ]);
 
+        //find and save id related
+        let roleNumber;
+        roleTable[0].forEach(element => {
+            if (element.title === response.employeeRole) {
+                roleNumber = element.id;
+            }
+        })
+
+        //find and save id related
+        let managerNumber;
+        managerTable[0].forEach(element => {
+            if (element.manager_name === response.employeeManger) {
+                managerNumber = element.id;
+            }
+        })
+
+        await connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+            VALUES ("${response.employeeFirstName}","${response.employeeLastName}",${roleNumber},${managerNumber})`);
+
+        connection.end();
         //get details and uploaded into database
         console.log(`added ${response.employeeFirstName} ${response.employeeLastName} with role ${response.employeeRole} and manager ${response.employeeManger}`)
 
 
     } else if (response.addItem === "Add a department") {
-        //Map all name of deparmtments
-        let departmentArrayMapped = departmentArray.map(a => a.name);
+
+        const connection = await mysql.createConnection(connectionFile.connection);
+
+        //Get departments into an array
+        const departmentTableUpdated = await connection.query(
+            `SELECT department_name FROM department`);
+        let listOfDepartment = [];
+        departmentTableUpdated[0].forEach(element => {
+            listOfDepartment.push(element.department_name)
+        })
+
+        console.log("The current departments are: ", listOfDepartment)
 
         const response = await inquirer.prompt([
             {
@@ -67,7 +103,7 @@ async function add(roleArray, employeeArray, departmentArray) {
                 message: 'What is the department you want to add?',
                 //Validate if alread exists.
                 validate: (addedDepartment) => {
-                    if (!departmentArrayMapped.includes(addedDepartment)) {
+                    if (!listOfDepartment.includes(addedDepartment)) {
                         return true;
                     } else {
                         console.log(`(Error: This department already exists!)`);
@@ -76,11 +112,44 @@ async function add(roleArray, employeeArray, departmentArray) {
             }
         ])
 
+        await connection.query(`INSERT INTO department (department_name) 
+            VALUES ("${response.addedDepartment}")`);
+
+        const departmentTable = await connection.query(
+            `SELECT department_name FROM department`);
+        listOfDepartment = [];
+        departmentTable[0].forEach(element => {
+            listOfDepartment.push(element.department_name)
+        })
+        connection.end();
         console.log(`Department ${response.addedDepartment} was added`);
+        console.log("The updated departments list: ", listOfDepartment)
 
     } else if (response.addItem === "Add a role") {
-        let roleArrayMapped = roleArray.map(a => a.title);
-        let departmentArrayMapped = departmentArray.map(a => a.name);
+
+        const connection = await mysql.createConnection(connectionFile.connection);
+
+        //Get Role names into an array
+        const roleTable = await connection.query(
+            `SELECT title FROM role`);
+        let listOfRoleTitles = [];
+        roleTable[0].forEach(element => {
+            listOfRoleTitles.push(element.title)
+        })
+
+        //Get Role names into an array
+        const departmentTable = await connection.query(
+            `SELECT id, department_name FROM department`);
+        let listOfDepartments = [];
+        departmentTable[0].forEach(element => {
+            listOfDepartments.push(element.department_name)
+        })
+
+        const roles = await connection.query(
+            `SELECT title, salary, department_name 
+            FROM role
+            join department on role.department_id = department.id`)
+        console.table(roles[0]);
 
         const response = await inquirer.prompt([
             {
@@ -88,7 +157,7 @@ async function add(roleArray, employeeArray, departmentArray) {
                 name: 'roleName',
                 message: 'What is the new role called?',
                 validate: (roleName) => {
-                    if (!roleArrayMapped.includes(roleName)) {
+                    if (!listOfRoleTitles.includes(roleName)) {
                         return true;
                     } else {
                         console.log(`(Error: This role already exists!)`);
@@ -111,12 +180,30 @@ async function add(roleArray, employeeArray, departmentArray) {
                 type: 'list',
                 name: 'roleDepartment',
                 message: 'What is the department the new role is in?',
-                choices: departmentArrayMapped
+                choices: listOfDepartments
             }
         ]);
 
-        console.log(`The role ${response.roleName}, with salary ${response.roleSalary} was added to department ${response.roleDepartment}`);
+        // Get & Save ID given the department role.
+        let departmentID;
+        departmentTable[0].forEach(element => {
+            if (element.department_name === response.roleDepartment) {
+                departmentID = element.id;
+            }
+        })
+
+        await connection.query(`INSERT INTO role (title, salary, department_id) 
+            VALUES ("${response.roleName}", ${response.roleSalary}, ${departmentID})`);
+
+
+        console.log(`The role ${response.roleName} was added.`)
+        const roles2 = await connection.query(
+            `SELECT title, salary, department_name 
+            FROM role
+            join department on role.department_id = department.id`)
+        console.table(roles2[0]);
+
+        connection.end();
     }
 }
-
 module.exports.add = add;
--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-
 const inquirer = require("inquirer");
 const addFile = require(`./assets/js/add.js`)
 const viewFile = require(`./assets/js/view.js`)
-require('dotenv').config()
-
-const connection = {
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.USER,
-    password: process.env.PASS,
-    database: "employeeDB",
-}
-
-
 
 // let viewFile = require(`./assets/js/view.js`)
 
 //Code Function
 async function code() {
-
-    // await addFile.add();
-
-    await viewFile.view();
-
-
+    const response = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'task',
+            message: 'What do you want to do?',
+            choices: ["Add Stuff", "View Stuff", "Update/Delete Stuff"]
+        }
+    ]);
+    if (response.task === "Add Stuff") {
+        await addFile.add();
+    } else if (response.task === "View Stuff") {
+        await viewFile.view();
+    }
 }
 
 code()
-module.exports.connection = connection;
